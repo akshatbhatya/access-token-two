4cb8176b8acb50948eb2e7fc01381c9c575c4a42
akshat <akshatbhatya@gmail.com>
2024-04-13 22:39:13 +0530

    app is configure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
-import express from "express";
+import express from 'express';
 
-const app=express();
-app.use(express.static("public"));
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(
+  express.urlencoded({
+    limit: '16kb',
+  })
+);
+
+app.use(
+  express.json({
+    limit: '16kb',
+  })
+);
 
 export default app;
