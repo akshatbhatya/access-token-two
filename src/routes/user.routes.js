0f6cb04b682ca4301efbd88e52b2b5f74f493987
akshat <akshatbhatya@gmail.com>
2024-04-15 10:38:25 +0530

    add middle ware upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { userRegister } from "../controllers/user.controllers.js";
-
+import { upload } from "../middlewares/multer.middlewares.js";
 const router=Router();
 
-router.route("/register").post(userRegister);
+router.route("/register").post(upload(
+    
+        
+        [
+            {
+                name:"avtar",
+                maxCount:1
+            },
+            {
+                name:"coverAvtar",
+                maxCount:1
+            }
+        ]
+    
+),userRegister);
 
 export default router;
